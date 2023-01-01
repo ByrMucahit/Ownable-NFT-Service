3afc3b21e83e741849d365ffc05a9b911565ab68
mucahitbayar <m.mucahitbayar@gmail.com>
2023-01-01 11:38:49 +0300

    * Add new service to mint nft * Remove old package-lock

---
+++ services/minting/Minting.js
@@ -0,0 +1,10 @@
+import {useState} from 'React';
+
+
+const Minting = () => {
+    const [walletAddress, setWallet] = useState("");
+    const [status, setStatus] = useState("");
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [url, setUrl] = useState("");
+}
